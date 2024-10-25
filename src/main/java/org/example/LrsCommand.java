ec4bdf144370436a3b3d7072e47fbd2246eea433
Abdotarek442 <114480959+Abdotarek442@users.noreply.github.com>
2024-10-25 05:54:34 +0300

    uploading LrsCommand.java
    lr -s command

---
+++ src/main/java/org/example/LrsCommand.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class LsrCommand implements Command{
+    File dir;
+    LsrCommand(File dir){
+        this.dir = dir;
+    }
+
+    public File execute(){
+        File []files = dir.listFiles();
+        if (files != null){
+            List<String> names = new ArrayList<>();
+            int i = 0;
+            for (File file : files){
+                if ( file.isFile()){
+                    names.add(file.getName()+ " [FILE]");
+                }
+                else names.add(file.getName() + " [DIR]");
+            }
+            Collections.sort(names, Collections.reverseOrder());
+            for (String name : names){
+                System.out.println(name);
+            }
+        }
+        else {
+            System.out.println("The Directory is empty");
+        }
+        return dir;
+    }
+}
