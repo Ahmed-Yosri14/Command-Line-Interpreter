26b34468326e67efd51b57bb82034da8a645765c
Abdotarek442 <114480959+Abdotarek442@users.noreply.github.com>
2024-10-25 05:50:55 +0300

    Update CLI.java
    Adding mkdir, rmdir, ls -r, command, and I've added if conditions for the ls

--- src/main/java/org/example/CLI.java
+++ src/main/java/org/example/CLI.java
@@ -1,5 +1,5 @@
 package org.example;
-import java.io.*;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,28 +12,31 @@ public class CLI {
     }
 
 
-    public void executeCommand(String input) throws IOException {
+    public void executeCommand(String input) {
         String[] parts = input.split(" ");
-        Command command = null ;
-
-
+        Command command = null;
         String commandName = parts[0].toLowerCase();
         switch (commandName) {
             case "cd":
-                if (parts.length==1){
-                    command= new PwdCommand(dir);
-                }
-                else command = new CdCommand(dir,parts[1]);
+                command = new CdCommand(dir,parts[1]);
                 break;
             case "ls":
-                command = new LsCommand(dir);
+                if ( parts.length == 1)
+                    command = new LsCommand(dir);
+                else if (parts[1].equalsIgnoreCase("-r")){
+                    command = new LsrCommand(dir);
+                }
                 break;
             case "pwd":
                 command = new PwdCommand(dir);
                 break;
-            case "touch":
-                command = new TouchCommand(dir,parts[1]);
+            case "mkdir":
+                command = new MkdirCommand(dir, parts[1]);
                 break;
+            case "rmdir":
+                command = new RmdirCommand(dir, parts[1]);
+                break;
+
             default:
                 if (parts.length==1){
                     command = new CdCommand(dir,parts[0]);
