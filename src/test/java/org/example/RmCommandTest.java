0cc3a7f3a9bdf02534abad359d6b49dc1ddaa484
Mohameddshawky <147840781+Mohameddshawky@users.noreply.github.com>
2024-10-30 00:37:03 +0300

    test1

---
+++ src/test/java/org/example/RmCommandTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.File;
+import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class RmCommandTest {
+    @Test
+    void Test1() throws IOException {
+        String input = "NewDirectory";
+        String[] parts = {"mkdir", input};
+        File dir = new File(System.getProperty("user.dir"));
+        Command c;
+        c = new MkdirCommand(dir, parts);
+        dir=c.execute();
+        //assertTrue(out1.contains(input));
+        RmdirCommand r=new RmdirCommand(dir,input);
+        dir=r.execute();
+       LsCommand ls = new LsCommand(dir,false);
+        ArrayList<String> out = ls.Get_Output();
+        assertFalse(out.contains(input));
+    }
+}
