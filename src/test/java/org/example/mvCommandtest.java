b56822108f246d5a7480d078b923ff80ed81acd6
Mohameddshawky <147840781+Mohameddshawky@users.noreply.github.com>
2024-10-31 00:09:05 +0300

    add newtest in mv

--- src/test/java/org/example/mvCommandtest.java
+++ src/test/java/org/example/mvCommandtest.java
@@ -25,4 +25,22 @@ public class mvCommandtest {
         List<String> out = ls.getOutput();
         assertTrue(out.contains(Target)&&!out.contains(input));
     }
+    @Test
+    void Test2() throws IOException {
+        String input = "newtest",Target="..\\";
+        String[] parts = {"mkdir", input};
+        String[] parts2 = {"cd", Target};
+        File dir = new File(System.getProperty("user.dir"));
+        Command c;
+        c = new MkdirCommand(dir, parts);
+        dir = c.execute();
+        Mv m=new Mv(dir,input,Target);
+        dir=m.execute();
+        CdCommand c1=new CdCommand(dir,parts2);
+        dir=c1.execute();
+        LsCommand ls = new LsCommand(dir,false);
+        List<String> out = ls.getOutput();
+        assertTrue(out.contains(input));
+    }
+
 }
